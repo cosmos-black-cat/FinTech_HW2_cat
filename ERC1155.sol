8270baaa47ac9602218d5b4d5fb4adb4194e3165
cosmos-black-cat <120732049+cosmos-black-cat@users.noreply.github.com>
2023-10-20 06:27:18 +0800

    Add files via upload

--- ERC1155.sol
+++ ERC1155.sol
@@ -10,6 +10,7 @@ contract GameItems is ERC1155 {
     uint256 public constant THORS_HAMMER = 2;
     uint256 public constant SWORD = 3;
     uint256 public constant SHIELD = 4;
+    uint256 public constant COSMOS_BLACK_CAT = 5;
 
     constructor() public ERC1155("ipfs://QmZ9JQ4wQ797k96sPgc31NFV8QfJqA2wWpht7thzUi3P5D/{id}.json") {
         _mint(msg.sender, GOLD, 10**18, "");
@@ -17,5 +18,6 @@ contract GameItems is ERC1155 {
         _mint(msg.sender, THORS_HAMMER, 1, "");
         _mint(msg.sender, SWORD, 10**9, "");
         _mint(msg.sender, SHIELD, 10**9, "");
+        _mint(msg.sender, COSMOS_BLACK_CAT, 1, "");
     }
 }
